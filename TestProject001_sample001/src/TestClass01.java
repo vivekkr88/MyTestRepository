30ab8b743a56c3e1b0d9233c9e86a9077416aa8f
vivekkr88 <vivekkr88@gmail.com>
2015-01-16 00:33:45 +0530

    My First Commit Change

---
+++ TestProject001_sample001/src/TestClass01.java
@@ -0,0 +1,13 @@
+
+public class TestClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloMoto");
+		int a = 1;
+		int b = 2;
+		int c = a+b;
+		System.out.println(c);
+	}
+
+}
